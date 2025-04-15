edd408666e433644ece9e294a88db8a58014dd84
Devemusab <musabkabirbkr@gmail.com>
2025-04-15 02:17:58 -0700

    first commit

---
+++ JS.js
@@ -0,0 +1,6 @@
+var trigger = document.getElementById("bar");
+var header = document.getElementById("mobile-view");
+
+trigger.addEventListener('click', function(){
+    header.classList.toggle('show')
+})
